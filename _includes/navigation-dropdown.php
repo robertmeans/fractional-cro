0a851e2d4bf4e40b7c00914b50e2a16df612cb1b
Evergreen Web Design <info@evergreenwebdesign.com>
2021-03-07 21:15:15 -0700

    fiddling with nav
    adding dropdown for future use.

--- _includes/navigation-dropdown.php
+++ _includes/navigation-dropdown.php
@@ -5,12 +5,26 @@
     </div>
     <nav>
       <div class="nav-mobile">
-        <a id="nav-toggle"><span></span></a>
+      	<a id="nav-toggle"><span></span></a>
       </div>
       <ul class="nav-list">
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
-        <li><a href="#clients">Clients</a></li>
+        
+
+
+        <li><a href="#clients">dropdown</a>
+          <ul class="nav-dropdown">
+          <li><a href="#clients">something 1</a></li>
+          <li><a href="#clients">something 2</a></li>
+          <li><a href="#clients">something 3</a></li>
+
+          </ul>
+        </li>
+
+
+
+
         <li><a id="myBtn">Contact</a></li> 
         <li class="in"><a href="https://www.linkedin.com/in/christiansolomine/" target="_blank"><img class="social" src="_images/linkedin.png"></a></li>       
       </ul>
